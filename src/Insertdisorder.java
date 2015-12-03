24ab4c3299bfff23ef38a7bbd2de4dba8a619a29
an92 <an.yi910525@gmail.com>
2015-12-03 18:19:38 +1100

    update a program that can find data from seq

---
+++ src/Insertdisorder.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Insertdisorder {
+    @SuppressWarnings("resource")
+	public static void main(String[] args) {       
+    	    String driver = "com.mysql.jdbc.Driver";
+    	    String url = "jdbc:mysql://localhost:3306/secretepdb";
+    	    String username = "root";
+    	    String password = "admin";
+    	    Connection conn = null;
+    	    Statement stmt = null;  	    
+    	     String Id=null;
+    	    String filepath="C:/Users/yia/Data/DB_pred";
+    	    String filepath_pred="C:/Users/yia/Data/DB";
+    	    String sql="";
+    	 try {       
+    		   Class.forName(driver);    
+    		   conn = DriverManager.getConnection(url, username, password);
+    		   stmt = conn.createStatement();
+               System.out.println("yes");
+         } catch (Exception e) {
+             System.out.print("MYSQL ERROR:" + e.getMessage());
+         }
+    	 try{
+    		 File file = new File(filepath);
+    		 File file_pred=new File(filepath_pred);
+    		 System.out.println(file.list().length);
+    		 if (file.isDirectory()) {     
+                 String[] filelist;
+                 String[] filelist_pred;
+                 String str="";
+                 String str_1="";
+                 filelist= new String[file.list().length];
+                 filelist_pred= new String[file_pred.list().length]; 
+                 filelist=file.list();
+                 filelist_pred=file_pred.list();
+                // for (int i = 0; i < filelist.length; i++) { 
+                  for (int i = 0; i < 1; i++) {
+                	 //File readfile = new File(filepath + "\\" + filelist[i]);  
+                	 File readfile = new File("C:/Users/yia/Data/DB_pred/1.pred");  
+                         if (!readfile.isDirectory())  {
+                        	 String ss=readfile.getName();                        	 
+                        	 Id=ss.substring(0, ss.indexOf("."));;//表Protein中的ID号
+                        	 BufferedReader  br = new BufferedReader(new FileReader(readfile));
+                        	 String s = null;
+                        	 while(!(s = br.readLine()).equals("Prediction Scores:") ){ 
+                        		     str_1+=s+";";
+                        		}
+						String str_2 = str_1.substring(str_1.indexOf(":") + 2, str_1.lastIndexOf(";"));// get
+																										// position
+						for (int m = 0; m < filelist_pred.length; m++) {
+							File pred = new File(filepath_pred + "\\" + filelist_pred[m]);
+							String pred_1 = pred.getName();
+							str_1 = pred_1.substring(0, pred_1.indexOf("."));
+							if (str_1.equals(Id)) {// 找到相同的文件
+								BufferedReader b = new BufferedReader(new InputStreamReader(new FileInputStream(pred)));
+								byte[] data = new byte[1024];
+								FileInputStream bbb = new FileInputStream(pred);
+								int a = bbb.read(data);
+								int[][] index = new int[4][2];
+								System.out.println(str_2);
+								String[] str_3 = str_2.split(";");
+								String[][] sstmp = new String[str_3.length-1][2];
+								// String ssss = new String(data,44,150);
+								String[] tmp = new String[str_3.length];
+								for (int q = 0; q < str_3.length - 1; q++) {
+									String stmp = str_3[q];
+									sstmp[q] = stmp.split("-");
+								}
+								for (int k = 0; k < sstmp.length; k++) {
+									int dis = Integer.parseInt(sstmp[k][1]) - Integer.parseInt(sstmp[k][0]);
+									String ssss = new String(data, Integer.parseInt(sstmp[k][0]) - 1, dis);
+									tmp[k] = ssss;
+									System.out.println(tmp[k]);
+								}
+							}
+						}
+					}
+                         
+         			  /* sql = "insert into protein(UniprotID,Name,Evidence,MolecularWeight,Function,Sequence,Length,altUniprotACC,DBid,Organism,Gene,allNames,flagType)values"
+         						+ "(\""+ uniprotId +"\",\""+name+"\",\"\",\""+molecalarweight+"\",\""+function+"\",\""+sequence+"\",\""+length+"\",\""+alt+"\",\"\",\""+organism+"\",\""+gene+"\",\""+names+"\",\"T3_blast\")";
+         				stmt.execute(sql);*/
+                 		// br.close();
+                 }	    	    
+			 }          
+    	 }catch(Exception e){
+    		 System.out.println(e);
+    		 
+    	 	}
+    }
+}
---
+++ src/Runcmd.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yia
+ *从数据库中读取数据，并使用jar文件运行，写成不同的文件，使用java程序运行cmd程序
+ */
+public class Runcmd {
+	
+	public static void mysqlConnection() throws SQLException, IOException{  //从数据库中读取数据并生成了文件
+		String driver = "com.mysql.jdbc.Driver";
+	    String url = "jdbc:mysql://localhost:3306/secretepdb";
+	    String username = "root";
+	    String password = "admin";
+	    Connection conn = null;
+	    Statement stmt = null;  	    
+	    String sql="";
+	    try {       
+	    		Class.forName(driver);    
+	    		conn = DriverManager.getConnection(url, username, password);
+	    		stmt = conn.createStatement();
+	    		System.out.println("yes");
+	    	} catch (Exception e) {
+	    			System.out.print("MYSQL ERROR:" + e.getMessage());
+	    	}
+		   
+	       sql = "select * from protein";//查询语句  
+	       ResultSet rs = stmt.executeQuery(sql);
+		   List<String> list=new ArrayList<String>();
+		   while (rs.next()) {
+		    list.add(rs.getString(1)+";"+rs.getString(7)); 
+		    //list.add(rs.getString(7));
+		   }  
+		   if(list != null && list.size()>0){
+		          String[] arr=new String[list.size()];//创建一个和list长度一样的数组  
+		          for(int i=0;i<list.size();i++){  
+		        	  arr[i]=list.get(i);//数组赋值了。  
+		          }  
+		          for(int i=0;i<arr.length;i++){  
+		              //System.out.println(arr[i]); 
+		              String[] str=arr[i].split(";");
+			          String strss=str[0];//Id
+			          String str_1=str[1];//sequence
+			          BufferedWriter bw = new BufferedWriter(new FileWriter("C:/Users/yia/Downloads/DB/" + strss + ".fasta"));
+			          bw.write(str_1);
+			      	  bw.close();
+		          }  
+		   }  
+		}  
+	
+	 public static  void openExe(String dir) {
+		  Runtime rn = Runtime.getRuntime();
+		  try {
+		   Process p=rn.exec(dir);
+		   if (p.waitFor() != 0) {
+		        if (p.exitValue() == 1)//p.exitValue()==0表示正常结束，1：非正常结束     
+		            System.err.println("命令执行失败!"); 
+		   }
+		  } catch (Exception e) {
+		   System.out.println("Error!"+e);
+		  }
+		 }
+	  public static void main(String[] args) throws SQLException, IOException { 
+		  mysqlConnection();
+		  String filepath="C:/Users/yia/Downloads/DB/";	
+		  String filepath_pred="C:\\Users/yia/Downloads/DB_pred/";
+		  File file = new File(filepath);
+		  String name="";
+		  if (file.isDirectory()) {     
+              String[] filelist;
+              filelist= new String[file.list().length];
+              filelist=file.list();
+              for (int i = 0; i < filelist.length; i++) {     
+            	  File readfile = new File(filepath + "\\" + filelist[i]);
+            	  if (!readfile.isDirectory())  {
+                 	 String ss=readfile.getName();                        	 
+                 	 name=ss.substring(0, ss.indexOf("."));;//name是表protein中的ID，也就是文件的名称。
+                 	 BufferedReader  br = new BufferedReader(new FileReader(readfile));
+                 	 System.out.println(readfile);
+                 	 System.out.println(name);
+                 	 String s = null;
+                 	 while((s = br.readLine()) != null){     
+            	  	String dir="cmd /c java -jar C:\\Users/yia/Downloads/VSL2/VSL2.jar -s:"+readfile+" -w:1 > "+filepath_pred+name+".pred";
+            	  	openExe(dir);
+                 	 }
+            	  }
+              }
+	  }
+	  }
+}
