01d3b13044623d6f5e7433907ae5e1ac308ecce4
an92 <an.yi910525@gmail.com>
2016-02-22 18:49:40 +1100

    更新了上传数据库的代码

--- src/FindID.java
+++ src/FindID.java
@@ -11,7 +11,7 @@ public class FindID {
 	 public static void main(String[] args) throws Exception{
 		 BufferedReader br = null;
 		 int num = 0;
-		File file = new File("C:/Users/yia/Google 云端硬盘/Server_Paper/data/dabase_data/T4_2_id.txt");
+		File file = new File("C:/Users/yia/Google 云端硬盘/Server_Paper/data/dabase_data/T4/T4_NCBI_id.txt");
 		 List<String> array = new ArrayList<String>();
 		 if (file.exists()) {
 	      		file.delete();
@@ -24,7 +24,7 @@ public class FindID {
 		 FileWriter fw = new FileWriter(file.getAbsoluteFile(),true);
   		 BufferedWriter bw = new BufferedWriter(fw);
 		 try{
-			 br = new BufferedReader(new FileReader("C:/Users/yia/Google 云端硬盘/Server_Paper/data/dabase_data/T4_full_new.fasta"));
+			 br = new BufferedReader(new FileReader("C:/Users/yia/Google 云端硬盘/Server_Paper/data/dabase_data/T4/T4_NCBi.fasta"));
 			 
 			 String s = null;
 			 while((s = br.readLine()) != null){
